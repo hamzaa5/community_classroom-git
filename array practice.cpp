e904abf6379db68761328096f4c6c417389a92fa
HamzaT <htariq911009@gmail.com>
2022-11-02 00:45:36 -0700

    This is a cpp program

---
+++ array practice.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+void fun()
+{
+	int num [2][2] = {{1,2},{2,1}};
+	for (int r=0; r<2; r++)
+	{
+		for (int c=0; c<2; c++)
+		{
+			cout<<num[r][c]<<" ";
+		} 
+		cout<<endl;
+	}
+}
+
+int main()
+{
+	fun();
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	
+	int a [4][4] = {{1,2,3,4},{5,6,7,8},{1,2,3,4},{5,6,7,8}};
+	for (int r=0; r<=3; r++)
+	{
+		for (int c=0; c<=3; c++)
+		{
+			cout<<a[r][c]<<" ";
+		}
+	cout<<endl;
+	}
+	
+	
+	
+	
+	return 0;
+}
