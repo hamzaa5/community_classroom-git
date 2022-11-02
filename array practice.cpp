4dce853a770e0be31e550f0e50a6bc7c8d285b5c
HamzaT <htariq911009@gmail.com>
2022-11-02 00:50:12 -0700

    I added this program

--- array practice.cpp
+++ array practice.cpp
@@ -1,38 +1,50 @@
-#include<iostream>
-using namespace std;
+//#include<iostream>
+//using namespace std;
+//
+//void fun()
+//{
+//	int num [2][2] = {{1,2},{2,1}};
+//	for (int r=0; r<2; r++)
+//	{
+//		for (int c=0; c<2; c++)
+//		{
+//			cout<<num[r][c]<<" ";
+//		} 
+//		cout<<endl;
+//	}
+//}
+//
+//int main()
+//{
+//	fun();
+//	cout<<endl;
+//	cout<<endl;
+//	cout<<endl;
+//	
+//	int a [4][4] = {{1,2,3,4},{5,6,7,8},{1,2,3,4},{5,6,7,8}};
+//	for (int r=0; r<=3; r++)
+//	{
+//		for (int c=0; c<=3; c++)
+//		{
+//			cout<<a[r][c]<<" ";
+//		}
+//	cout<<endl;
+//	}
+//	
+//	
+//	
+//	
+//	return 0;
+//}
 
-void fun()
-{
-	int num [2][2] = {{1,2},{2,1}};
-	for (int r=0; r<2; r++)
-	{
-		for (int c=0; c<2; c++)
-		{
-			cout<<num[r][c]<<" ";
-		} 
-		cout<<endl;
-	}
-}
+
+//after testing
+
+#include <iostream>
+using namespace std;
 
 int main()
 {
-	fun();
-	cout<<endl;
-	cout<<endl;
-	cout<<endl;
-	
-	int a [4][4] = {{1,2,3,4},{5,6,7,8},{1,2,3,4},{5,6,7,8}};
-	for (int r=0; r<=3; r++)
-	{
-		for (int c=0; c<=3; c++)
-		{
-			cout<<a[r][c]<<" ";
-		}
-	cout<<endl;
-	}
-	
-	
-	
-	
+	cout<<"GitClassroom";
 	return 0;
 }
